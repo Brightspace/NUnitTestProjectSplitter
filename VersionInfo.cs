57a30aad8155c80461f24260062addb5e22bf2f6
sawang <sara.wang@d2l.com>
2017-06-22 10:08:49 -0400

    fix verison

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion( "1.0.5" )]
-[assembly: AssemblyFileVersion( "1.0.5" )]
-[assembly: AssemblyInformationalVersion( "1.0.5-dev" )]
+[assembly: AssemblyVersion( "0.0.0" )]
+[assembly: AssemblyFileVersion( "0.0.0" )]
+[assembly: AssemblyInformationalVersion( "0.0.0-dev" )]
