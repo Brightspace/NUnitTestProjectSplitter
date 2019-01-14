0f685c3fe3b3993e96934a9c76a7e1f4c9f2bbae
Owen Smith <Owen.Smith@d2l.com>
2019-01-14 11:08:31 -0500

    don't assert specific nunit version

--- NUnitTestProjectSplitter/NUnitFrameworkReferenceChecker.cs
+++ NUnitTestProjectSplitter/NUnitFrameworkReferenceChecker.cs
@@ -6,33 +6,17 @@ namespace NUnitTestProjectSplitter {
 
 	internal static class NUnitFrameworkReferenceChecker {
 
-		private static readonly AssemblyName m_expectedNUnitFramework =
-			typeof( NUnit.Framework.TestFixtureAttribute ).Assembly.GetName();
+        private const string NUNIT_ASSEMBLY_NAME = "nunit.framework";
 
 		public static bool ReferencesNUnitFramework( Assembly assembly ) {
 
-			string expectedAssemblyName = m_expectedNUnitFramework.Name;
-			Version expectedVersion = m_expectedNUnitFramework.Version;
+            AssemblyName[] assmeblies = assembly.GetReferencedAssemblies();
 
-			AssemblyName[] nunitAssemblies = assembly.GetReferencedAssemblies()
-				.Where( x => x.Name.Equals( expectedAssemblyName ) )
-				.ToArray();
+            if( !assmeblies.Any( a => a.Name == NUNIT_ASSEMBLY_NAME ) ) {
+                return false;
+            }
 
-			if( !nunitAssemblies.Any() ) {
-				return false;
-			}
-
-			foreach( AssemblyName nunitAssembly in nunitAssemblies ) {
-
-				Version version = nunitAssembly.Version;
-				if( !version.Equals( expectedVersion ) ) {
-
-					string msg = $"Expected { expectedAssemblyName } version { expectedVersion }, but found { version } in {assembly.GetName().Name}";
-					throw new UnexpectedNUnitVersionException( msg );
-				}
-			}
-
-			return true;
+            return true;
 		}
 	}
 }
